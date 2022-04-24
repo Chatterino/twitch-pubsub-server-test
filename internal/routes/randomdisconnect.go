844905f968c70f5fc0a688ed7d15cba9a60d442e
pajlada <rasmus.karlsson@pajlada.com>
2022-04-24 15:52:49 +0200

    Release v1.0.0 (#1)

--- internal/routes/randomdisconnect.go
+++ internal/routes/randomdisconnect.go
@@ -9,7 +9,7 @@ import (
 )
 
 func RandomDisconnect(c *websocket.Conn, r *http.Request) {
-	ctx, _ := context.WithTimeout(r.Context(), 1500*time.Millisecond)
+	ctx, _ := context.WithTimeout(r.Context(), 1*time.Second)
 
 	for {
 		doBreak := defaultHandler(ctx, c, r)
